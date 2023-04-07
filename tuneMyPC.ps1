080526d0db9048f0e7f1e8ef1bfee5d7ff29d94a
Alex McPadden <42503691+alexmczz@users.noreply.github.com>
2023-04-06 22:23:11 -0400

    Update tuneMyPC.ps1

--- tuneMyPC.ps1
+++ tuneMyPC.ps1
@@ -1,49 +1,123 @@
-﻿$date = Get-Date
-$user_update = Read-Host "Would you like to install windows update module? y|n: "
+$date = Get-Date 
 
-if($user_update.ToLower() -eq 'y'){
-    
-    install-module PSwindowsUpdate
-    Update-MpSignature
-    Write-Host 'Windows update Module Installed $date' -ForegroundColor Yellow 
-}else{
-    Write-Host "oki :) " -ForegroundColor Green
+
+function step_by_step_tune{
+    <#
+    ******************************************************
+    step by step tune up on pc, starts with installing
+    modules needed through tune up, and asks you manually
+    what scans or updates you would like to do.
+    ******************************************************
+    #>
+    $user_update = Read-Host "Would you like to install windows update module? 
+    y|n: "
+
+    if($user_update.ToLower() -eq 'y'){
+        #installs windows update module and security scans
+
+        install-module PSwindowsUpdate
+        Update-MpSignature
+        Write-Host 'Windows update Module Installed '$date -ForegroundColor 
+    Yellow 
+    }else{
+        Write-Host "oki :) " -ForegroundColor Green
+    }
+
+
+
+    $install_update = Read-Host "Would you like to contiue? y|n: "
+    #retrieves windows updates and installs them
+    if($install_update.ToLower() -eq 'y'){
+        Write-Host 'Retrieving update...' -ForegroundColor Yellow
+        Get-WindowsUpdate
+        Write-Host 'Update Recieved!' -ForegroundColor Green
+        Write-Host 'Update Start' -ForegroundColor White -BackgroundColor Gray
+        Install-WindowsUpdate
+        write-host 'Update Complete!' -ForegroundColor Yellow
+    }else{
+        Write-host "oki :)" -ForegroundColor Green 
+    }
+
+
+
+    $fileSys_scan = Read-Host 'Would you like to tune up your system? y|n: '
+    if($fileSys_scan.ToLower() -eq 'y'){
+        Write-Host 'FileSystem scan start '$date -ForegroundColor White 
+    -BackgroundColor Yellow
+    #scans file system defrags and trims C drive(c is most likely the boot sector)
+        sfc /scannow
+        Optimize-Volume -DriveLetter C -Analyze -Verbose
+        Optimize-Volume -DriveLetter C -Defrag -Verbose
+        Optimize-Volume -DriveLetter C -ReTrim -Verbose
+        Write-Host 'FileSystem scan complete' -ForegroundColor white 
+    -BackgroundColor Green
+    }else{
+        Write-Host 'oki' -ForegroundColor Yellow
+    }
+
+
+
+    $get_secure = Read-Host "Would you like to perform quick security scan? 
+    y|n: "
+    if($get_secure.ToLower() -eq 'y'){
+        $get_sec_type = Read-Host "Would you like a quick or full scan? 1|2: "
+        if($get_sec_type -eq '1'){
+            #quick scan
+            Start-MpScan -ScanType QuickScan
+        }else{
+            #full scan
+            Start-MpScan -ScanType FullScan
+        }
+        Write-Host "Scan Finished" -ForegroundColor Green
+    }else{
+        write-host 'oh oki ' -ForegroundColor Red
+    }
 }
 
-$install_update = Read-Host "Would you like to contiue? y|n: "
+function automate_tune{
+    <#
+    ******************************************************
+    automated integrity scans, updates and, security scans
+    ******************************************************
+    #>
 
-if($install_update.ToLower() -eq 'y'){
+    #grabs and runs windows updates
     Write-Host 'Retrieving update...' -ForegroundColor Yellow
     Get-WindowsUpdate
     Write-Host 'Update Recieved!' -ForegroundColor Green
     Write-Host 'Update Start' -ForegroundColor White -BackgroundColor Gray
     Install-WindowsUpdate
     write-host 'Update Complete!' -ForegroundColor Yellow
-}else{
-    Write-host "oki :)" -ForegroundColor Green 
-}
-
-
-$fileSys_scan = Read-Host 'Would you like to tune up your system? y|n: '
-if($fileSys_scan.ToLower() -eq 'y'){
-    Write-Host 'FileSystem scan start' -ForegroundColor White -BackgroundColor Yellow
+    Write-Host 'FileSystem scan start '$date -ForegroundColor White 
+    -BackgroundColor Yellow
+    #scans file system defrags and trims C drive(c is most likely the boot sector)
     sfc /scannow
     Optimize-Volume -DriveLetter C -Analyze -Verbose
     Optimize-Volume -DriveLetter C -Defrag -Verbose
     Optimize-Volume -DriveLetter C -ReTrim -Verbose
-    Write-Host 'FileSystem scan complete' -ForegroundColor white -BackgroundColor Green
-}else{
-    Write-Host 'oki' -ForegroundColor Yellow
+    Start-MpScan -ScanType FullScan
+    Write-Host "Scan Finished" -ForegroundColor Green
+    ipconfig /flushdns
+
 }
 
-$get_secure = Read-Host "Would you like to perform quick security scan? y|n: "
-if($get_secure.ToLower() -eq 'y'){
-    $get_sec_type = Read-Host "Would you like a quick or full scan? 1|2: "
-    if($get_sec_type -eq '1'){
-        Start-MpScan -ScanType QuickScan
+$currentuser = $env:USERNAME
+$currentuserprofile = $env:USERPROFILE
+
+if (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "User '$currentuser' running script as administrator from '$currentuserprofile'."
+    $user_response = read-host "Press Enter to continue or any key to quit:"
+    if($user_response -eq ''){
+        $choice = read-host "Press Enter 1 for auto tuneup or 2 for manual tuneup"
+        if($choice -eq '1'){
+            step_by_step_tune
+        }else{
+            automate_tune
+        }
     }else{
-    Start-MpScan -ScanType FullScan
+        Write-Host "Good bye!" -ForegroundColor Yellow
     }
-}else{
-    write-host 'oh oki ' -ForegroundColor Red
+} else {
+    Write-Warning "User '$currentuser' does not have administrator privileges. Please run this script as an administrator."
 }
+
