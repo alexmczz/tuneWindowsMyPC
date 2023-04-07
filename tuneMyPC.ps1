8f8ef811640e8489d848c8b4a7b0b0855e554a27
Alex McPadden <42503691+alexmczz@users.noreply.github.com>
2023-04-06 22:30:16 -0400

    Update tuneMyPC.ps1

--- tuneMyPC.ps1
+++ tuneMyPC.ps1
@@ -1,7 +1,6 @@
 $date = Get-Date 
 
-
-function step_by_step_tune{
+function step_by_step_tune {
     <#
     ******************************************************
     step by step tune up on pc, starts with installing
@@ -9,72 +8,87 @@ function step_by_step_tune{
     what scans or updates you would like to do.
     ******************************************************
     #>
-    $user_update = Read-Host "Would you like to install windows update module? 
-    y|n: "
+    $user_update = Read-Host "Would you like to install windows update module? y|n: "
 
-    if($user_update.ToLower() -eq 'y'){
-        #installs windows update module and security scans
-
-        install-module PSwindowsUpdate
-        Update-MpSignature
-        Write-Host 'Windows update Module Installed '$date -ForegroundColor 
-    Yellow 
-    }else{
-        Write-Host "oki :) " -ForegroundColor Green
+    if($user_update.ToLower() -eq 'y') {
+        try {
+            # installs windows update module and security scans
+            install-module PSwindowsUpdate -ErrorAction Stop
+            Update-MpSignature -ErrorAction Stop
+            Write-Host 'Windows update Module Installed ' $date -ForegroundColor Yellow 
+        }
+        catch {
+            Write-Error "An error occurred while installing windows update module or updating signatures: $_"
+        }
+    } else {
+        Write-Host "oki :)" -ForegroundColor Green
     }
 
+    $install_update = Read-Host "Would you like to continue? y|n: "
 
-
-    $install_update = Read-Host "Would you like to contiue? y|n: "
-    #retrieves windows updates and installs them
-    if($install_update.ToLower() -eq 'y'){
-        Write-Host 'Retrieving update...' -ForegroundColor Yellow
-        Get-WindowsUpdate
-        Write-Host 'Update Recieved!' -ForegroundColor Green
-        Write-Host 'Update Start' -ForegroundColor White -BackgroundColor Gray
-        Install-WindowsUpdate
-        write-host 'Update Complete!' -ForegroundColor Yellow
-    }else{
+    if($install_update.ToLower() -eq 'y') {
+        try {
+            Write-Host 'Retrieving update...' -ForegroundColor Yellow
+            Get-WindowsUpdate -ErrorAction Stop
+            Write-Host 'Update Received!' -ForegroundColor Green
+            Write-Host 'Update Start' -ForegroundColor White -BackgroundColor Gray
+            Install-WindowsUpdate -ErrorAction Stop
+            Write-Host 'Update Complete!' -ForegroundColor Yellow
+        }
+        catch {
+            Write-Error "An error occurred while retrieving or installing updates: $_"
+        }
+    } else {
         Write-host "oki :)" -ForegroundColor Green 
     }
 
-
-
     $fileSys_scan = Read-Host 'Would you like to tune up your system? y|n: '
-    if($fileSys_scan.ToLower() -eq 'y'){
-        Write-Host 'FileSystem scan start '$date -ForegroundColor White 
-    -BackgroundColor Yellow
-    #scans file system defrags and trims C drive(c is most likely the boot sector)
-        sfc /scannow
-        Optimize-Volume -DriveLetter C -Analyze -Verbose
-        Optimize-Volume -DriveLetter C -Defrag -Verbose
-        Optimize-Volume -DriveLetter C -ReTrim -Verbose
-        Write-Host 'FileSystem scan complete' -ForegroundColor white 
-    -BackgroundColor Green
-    }else{
+    if($fileSys_scan.ToLower() -eq 'y') {
+        try {
+            Write-Host 'FileSystem scan start ' $date -ForegroundColor White -BackgroundColor Yellow
+            # scans file system defrags and trims C drive(c is most likely the boot sector)
+            sfc /scannow
+            Optimize-Volume -DriveLetter C -Analyze -Verbose
+            Optimize-Volume -DriveLetter C -Defrag -Verbose
+            Optimize-Volume -DriveLetter C -ReTrim -Verbose
+            Write-Host 'FileSystem scan complete' -ForegroundColor White -BackgroundColor Green
+        }
+        catch {
+            Write-Error "An error occurred while scanning and optimizing the file system: $_"
+        }
+    } else {
         Write-Host 'oki' -ForegroundColor Yellow
     }
 
-
-
-    $get_secure = Read-Host "Would you like to perform quick security scan? 
-    y|n: "
-    if($get_secure.ToLower() -eq 'y'){
+    $get_secure = Read-Host "Would you like to perform quick security scan? y|n: "
+    if($get_secure.ToLower() -eq 'y') {
         $get_sec_type = Read-Host "Would you like a quick or full scan? 1|2: "
-        if($get_sec_type -eq '1'){
-            #quick scan
-            Start-MpScan -ScanType QuickScan
-        }else{
-            #full scan
-            Start-MpScan -ScanType FullScan
+        if($get_sec_type -eq '1') {
+            try {
+                # quick scan
+                Start-MpScan -ScanType QuickScan -ErrorAction Stop
+                Write-Host "Quick Scan Finished" -ForegroundColor Green
+            }
+            catch {
+                Write-Error "An error occurred while performing quick scan: $_"
+            }
+        } else {
+            try {
+                # full scan
+                Start-MpScan -ScanType FullScan -ErrorAction Stop
+                Write-Host "Full Scan Finished" -ForegroundColor Green
+            }
+            catch {
+                Write-Error "An error occurred while performing full scan: $_"
+            }
         }
-        Write-Host "Scan Finished" -ForegroundColor Green
-    }else{
+    } else {
         write-host 'oh oki ' -ForegroundColor Red
     }
 }
 
-function automate_tune{
+function automate_tune {
+
     <#
     ******************************************************
     automated integrity scans, updates and, security scans
